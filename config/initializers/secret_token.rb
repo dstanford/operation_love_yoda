cbc2c3b7b443bb3c6672396a82ffba31158d151f
Daniel Stanford <daniel@danielstanford.com>
2011-11-13 16:55:41 -0600

    added all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HugDoctor::Application.config.secret_token = 'a6127a0ea383ae8a0f323dec1223f757bcc4f4654ef1a94a06a65b554e7d32c61dd96ed7640c42f10fb6063f02f852e718652b1907ebe1d7009ee45ae2884a0f'
